6ea6b001ca464428c712eebd04b5e2c93f976906
skompalli <sskmp6@gmail.com>
2025-01-13 14:52:25 -0800

    fixing articles

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
 				<Route path="/about" element={<About />} />
 				<Route path="/projects" element={<Projects />} />
 				<Route path="/articles" element={<Articles />} />
-				<Route path="/:slug" element={<ReadArticle />} />
+				<Route path="/article/:slug" element={<ReadArticle />} />
 				<Route path="/contact" element={<Contact />} />
 				<Route path="*" element={<Notfound />} />
 			</Routes>
